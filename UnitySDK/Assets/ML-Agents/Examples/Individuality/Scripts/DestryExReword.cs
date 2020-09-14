616492dd2ef98d636e25cf7f8021db1ca5b591b3
Non_ <non.82738991@gmail.com>
2020-09-14 16:32:43 +0900

    Individualityフォルダを新たに作成
    オリジナルのGridWorldから分離

---
+++ UnitySDK/Assets/ML-Agents/Examples/Individuality/Scripts/DestryExReword.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestryExReword : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider collider)
+    {
+        //if (collider.gameObject.name == "exReword")
+        Destroy(this.gameObject);
+        //Debug.Log("Destroy：exReword");
+    }
+}
