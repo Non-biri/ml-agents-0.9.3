1ab3c81bf80f7ddc02bd1519234c42468b196799
Non_ <non.82738991@gmail.com>
2020-12-03 16:19:31 +0900

    Update PersonlityAgent.cs
    PersonlityAgentをHallwayAgent.csをもとにrey処理を追加中

--- UnitySDK/Assets/ML-Agents/Personality_Agent/Scripts/PersonlityAgent.cs
+++ UnitySDK/Assets/ML-Agents/Personality_Agent/Scripts/PersonlityAgent.cs
@@ -5,17 +5,75 @@ using MLAgents;
 
 public class PersonlityAgent : Agent
 {
-    /*
-    // Start is called before the first frame update
-    void Start()
+    public bool useVectorObs;
+    Rigidbody agentRB;
+    PersonlityAcademy academy;
+
+
+
+
+    public override void InitializeAgent()
     {
-        
+        base.InitializeAgent();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public override void CollectObservations()
     {
-        
+        if (useVectorObs)
+        {
+            float reyDistanse = 12f;
+            float[] reyAngles = { 20f, 60f, 90f, 120f, 160f };
+            string[] detectableObjects = { "Wall" };
+
+        }
+        base.CollectObservations();
     }
-    */
+
+
+    public void MoveAgent(float[] act)
+    {
+
+        Vector3 dirToGo = Vector3.zero;
+        Vector3 rotateDir = Vector3.zero;
+
+        if (brain.brainParameters.vectorActionSpaceType == SpaceType.continuous)
+        {
+            dirToGo = transform.forward * Mathf.Clamp(act[0], -1f, 1f);
+            rotateDir = transform.up * Mathf.Clamp(act[1], -1f, 1f);
+        }
+        else
+        {
+            int action = Mathf.FloorToInt(act[0]);
+            switch (action)
+            {
+                case 1:
+                    dirToGo = transform.forward * 1f;
+                    break;
+                case 2:
+                    dirToGo = transform.forward * -1f;
+                    break;
+                case 3:
+                    rotateDir = transform.up * 1f;
+                    break;
+                case 4:
+                    rotateDir = transform.up * -1f;
+                    break;
+            }
+        }
+        transform.Rotate(rotateDir, Time.deltaTime * 150f);
+        agentRB.AddForce(dirToGo * academy.agentRunSpeed, ForceMode.VelocityChange);
+    }
+
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        base.AgentAction(vectorAction, textAction);
+    }
+
+
+    public override void AgentReset()
+    {
+        base.AgentReset();
+    }
 }
