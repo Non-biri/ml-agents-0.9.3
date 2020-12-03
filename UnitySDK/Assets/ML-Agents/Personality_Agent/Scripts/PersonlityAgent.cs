6b83e93b0ed4886023ef6a5f0a56263a702884db
Non_ <non.82738991@gmail.com>
2020-12-03 11:02:46 +0900

    Update PersonlityAgent.cs
    github Desktop initial commit &
    initial PersonlityAgent

--- UnitySDK/Assets/ML-Agents/Personality_Agent/Scripts/PersonlityAgent.cs
+++ UnitySDK/Assets/ML-Agents/Personality_Agent/Scripts/PersonlityAgent.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using System.Linq;
+using MLAgents;
 
-public class Agent : MonoBehaviour
+public class PersonlityAgent : Agent
 {
+    /*
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +17,5 @@ public class Agent : MonoBehaviour
     {
         
     }
+    */
 }
